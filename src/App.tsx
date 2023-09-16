612306ae0cd111e6ec5c12fcd617a3b3be1bc3b8
siamsplash5 <siamsplash52@gmail.com>
2023-09-16 23:57:23 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "../src/assets/index.css";
+import Login from "./components/Login";
+
+function App() {
+    return <Login />;
+}
+
+export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <>
+            <div className="bg-green-500 rounded-xl">Hello World</div>
+        </>
+    );
+}
